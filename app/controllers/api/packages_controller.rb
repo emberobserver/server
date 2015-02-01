4a1d25435f1ffe961fc004b6da2783f1afe371af
superlou <lousimons@gmail.com>
2015-02-01 15:59:49 -0500

    Add index and show for packages

--- app/controllers/api/packages_controller.rb
+++ app/controllers/api/packages_controller.rb
@@ -1,2 +1,11 @@
 class API::PackagesController < ApplicationController
+  def index
+    @packages = Package.all
+    render json: @packages
+  end
+
+  def show
+    @package = Package.find(params[:id])
+    render json: @package
+  end
 end
