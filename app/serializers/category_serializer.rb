5a56f4edcd80d235efe11b8a522c65909585f5e6
Katie Gengler <katie@kmg.io>
2015-02-01 22:31:17 -0500

    Add description seeds to categories

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,3 +1,3 @@
 class CategorySerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id, :name, :description
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -20,5 +20,17 @@ Metric.create(
 
 Category.destroy_all
 
-Category.create(name: 'authentication')
-Category.create(name: 'ui components')
+categories = [
+  {name: 'Authentication', description: 'Addons for auth', package_ids: [1, 2, 3]},
+  {name: 'Components', description: 'Addons that provide a component', package_ids: [1, 2]},
+  {name: 'Styles', description: 'Addons that provide styles, css frameworks, preprocessors', package_ids: [3]},
+  {name: 'Testing', description: 'Addons related to testing', package_ids: [1, 2, 3]},
+  {name: 'Build tools', description: 'Addons related to preprocessing, broccoli plugins and dependencies of ember-cli', package_ids: [1, 2, 3]},
+  {name: 'Data', description: 'Addons related to ember-data or alternatives to ember-data', package_ids: []},
+  {name: 'Library wrappers', description: 'Addons that wrap third party libraries, jQuery plugins and the like', package_ids: [2]},
+  {name: 'Miscellaneous', description: 'The addons that don\'t fit into other categories', package_ids: [1, 2, 3]}
+]
+
+categories.each do |c|
+  Category.create(c)
+end
