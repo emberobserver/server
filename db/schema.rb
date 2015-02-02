d26dd8f75e3cea556f8ed2bf53d7c4025325b8c6
Phil Gengler <phil@pgengler.net>
2015-02-01 22:27:02 -0500

    Add 'description' column to 'categories' table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150202012631) do
+ActiveRecord::Schema.define(version: 20150202032604) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
   create_table "category_packages", force: :cascade do |t|
