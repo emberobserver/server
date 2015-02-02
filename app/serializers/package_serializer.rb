ea09b867ef8d4911ee18187aff7b27294dd10ff4
superlou <lousimons@gmail.com>
2015-02-02 16:15:58 -0500

    Include version info in package serializer

--- app/serializers/package_serializer.rb
+++ app/serializers/package_serializer.rb
@@ -1,3 +1,3 @@
 class PackageSerializer < ActiveModel::Serializer
-  attributes :id, :name, :npmjs_url, :repository_url
+  attributes :id, :name, :npmjs_url, :repository_url, :latest_version, :latest_version_date
 end
