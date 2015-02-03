bbce86f41279a9fbde4325d5436166945b80fb9c
superlou <lousimons@gmail.com>
2015-02-02 19:50:47 -0500

    Add description to package serializer

--- app/serializers/package_serializer.rb
+++ app/serializers/package_serializer.rb
@@ -1,3 +1,3 @@
 class PackageSerializer < ActiveModel::Serializer
-  attributes :id, :name, :npmjs_url, :repository_url, :latest_version, :latest_version_date
+  attributes :id, :name, :npmjs_url, :repository_url, :latest_version, :latest_version_date, :description
 end
