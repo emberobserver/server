60fbd45bb7e5d47e10808a0d8bb72a58c58dea39
Phil Gengler <phil@pgengler.net>
2015-02-05 17:25:11 -0500

    Remove routes for evaluations, metric, and reviews

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   resources :category_packages
 
   scope :api, defaults: { format: :json } do
-    resources :evaluations
-    resources :metrics
-    resources :reviews
     resources :packages
     resources :categories
     resources :keywords
