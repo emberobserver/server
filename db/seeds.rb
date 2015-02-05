659d4c24235c1879fdb0dbde55b0a28eaf81595c
Phil Gengler <phil@pgengler.net>
2015-02-05 17:53:03 -0500

    Remove Metric-related DB seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-Metric.destroy_all
-
-Metric.create(
-  name: 'Tests',
-  description: 'Are tests thorough and robust?'
-)
-
-Metric.create(
-  name: 'Readme',
-  description: 'Is there a complete readme covering topics pertinant to a new user?'
-)
-
 Category.destroy_all
 
 categories = [
