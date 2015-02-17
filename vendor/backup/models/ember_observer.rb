cb356f12ddf4ee05a3148aabe33d1f12f9c2aea9
Phil Gengler <phil@pgengler.net>
2015-02-16 21:04:17 -0500

    Configure 'backup' gem

---
+++ vendor/backup/models/ember_observer.rb
@@ -0,0 +1,34 @@
+# encoding: utf-8
+
+app_path="/srv/app/ember-site/server"
+Dotenv.load "#{app_path}/shared/.env"
+db_config=YAML.load_file("#{app_path}/shared/config/database.yml")['production']
+##
+# Backup Generated: ember_observer
+# Once configured, you can run the backup with the following command:
+#
+# $ backup perform -t ember_observer [-c <path_to_configuration_file>]
+#
+# For more information about Backup's components, see the documentation at:
+# http://meskyanichi.github.io/backup
+#
+Model.new(:ember_observer, 'Description for ember_observer') do
+
+  database PostgreSQL do |db|
+    db.name               = db_config['database']
+    db.username           = db_config['username']
+    db.password           = db_config['password']
+    db.host               = db_config['host']
+  end
+
+  store_with Local do |local|
+    local.path = '~/backups'
+    local.keep = 5
+  end
+
+  ##
+  # Bzip2 [Compressor]
+  #
+  compress_with Bzip2
+
+end
