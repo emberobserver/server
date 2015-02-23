7febff9cda43e38faec6ccbb9f335f5f48d5cbd0
Phil Gengler <phil@pgengler.net>
2015-02-22 22:57:57 -0500

    Include parent category ID and subcategory IDs in Category serializer

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,4 +1,8 @@
 class CategorySerializer < ApplicationSerializer
-  attributes :id, :name, :description
+  attributes :id, :name, :description, :parent_id, :categories
   has_many :addons, include: false
+
+  def categories
+    object.subcategories.map(&:id)
+  end
 end
