c6525228de6e743b1d79093dadc653fa8cc34396
Phil Gengler <phil@pgengler.net>
2015-02-26 21:01:49 -0500

    Rerun 'annotate'

--- app/models/addon_download.rb
+++ app/models/addon_download.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: addon_downloads
+#
+#  id        :integer          not null, primary key
+#  addon_id  :integer
+#  date      :date
+#  downloads :integer
+#
+
 class AddonDownload < ActiveRecord::Base
 	belongs_to :addon
 end
