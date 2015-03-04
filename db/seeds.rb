26881f72dff0313243a06d1f1bec3e7079206fd5
Phil Gengler <phil@pgengler.net>
2015-03-03 16:33:25 -0800

    Update seed data to put Components in the wide position

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,8 @@ categories = [
   {name: 'Authentication', description: 'Addons for auth', position: 1},
   {name: 'Styles', description: 'Addons that provide styles, css frameworks, preprocessors', position: 2},
   {name: 'Testing', description: 'Addons related to testing', position: 3},
-  {name: 'Build tools', description: 'Addons related to preprocessing, broccoli plugins and dependencies of ember-cli', position: 4},
-  {name: 'Components', description: 'Addons that provide a component', position: 5},
+  {name: 'Components', description: 'Addons that provide a component', position: 4},
+  {name: 'Build tools', description: 'Addons related to preprocessing, broccoli plugins and dependencies of ember-cli', position: 5},
   {name: 'Data', description: 'Addons related to ember-data or alternatives to ember-data', position:6},
   {name: 'Library wrappers', description: 'Addons that wrap third party libraries, jQuery plugins and the like', position:7},
   {name: 'Miscellaneous', description: 'The addons that don\'t fit into other categories', position: 8}
