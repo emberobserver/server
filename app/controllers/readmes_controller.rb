927ee97f451efd21b583c7fb572f30b303537be7
superlou <lousimons@gmail.com>
2015-03-17 20:26:23 -0400

    Return readme wrapped in array

--- app/controllers/readmes_controller.rb
+++ app/controllers/readmes_controller.rb
@@ -4,10 +4,11 @@ class ReadmesController < ApplicationController
     if params[:addon_id]
       readme = Addon.find(params[:addon_id]).github_stats.readme
       render json: {
-        readme: {
+        readmes: [{
           id: params[:addon_id],
+          addon_id: params[:addon_id],
           text: readme
-        }
+        }]
       }
     else
       head :bad_request
