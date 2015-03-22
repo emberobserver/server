dfe891522e9651fb23bbd785cf69f3418643a827
Phil Gengler <phil@pgengler.net>
2015-03-22 12:36:38 -0400

    When sending correction email, use supplied address as reply-to

--- app/mailers/correction_mailer.rb
+++ app/mailers/correction_mailer.rb
@@ -1,6 +1,6 @@
 class CorrectionMailer < ActionMailer::Base
 	def correction(args)
 		@correction = args
-		mail to: 'observers@emberobserver.com', from: 'Corrections <noreply@emberobserver.com>', subject: 'Ember Observer correction suggestion'
+		mail to: 'observers@emberobserver.com', from: 'Corrections <noreply@emberobserver.com>', reply_to: args[:email], subject: 'Ember Observer correction suggestion'
 	end
 end
