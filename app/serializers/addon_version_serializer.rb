32136bd00188bd9925f4ce2d337a995c5c186285
Phil Gengler <phil@pgengler.net>
2015-06-02 13:50:10 -0400

    Include ember-cli version number in AddonVersion responses

--- app/serializers/addon_version_serializer.rb
+++ app/serializers/addon_version_serializer.rb
@@ -1,4 +1,4 @@
 class AddonVersionSerializer < ApplicationSerializer
-	attributes :id, :version, :released, :addon_id
+	attributes :id, :version, :released, :addon_id, :ember_cli_version
   has_one :review
 end
