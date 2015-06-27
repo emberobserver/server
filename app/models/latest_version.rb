0de36e024a406396a8970aa12329f9346600d8d6
Phil Gengler <phil@pgengler.net>
2015-06-27 15:08:34 -0400

    Annotate LatestVersion

--- app/models/latest_version.rb
+++ app/models/latest_version.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: latest_versions
+#
+#  id         :integer          not null, primary key
+#  package    :string
+#  version    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class LatestVersion < ActiveRecord::Base
 	validates :package, uniqueness: true
 end
