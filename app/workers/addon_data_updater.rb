e707657418c0644c37d82057d673e8ea44f29a34
Phil Gengler <phil@pgengler.net>
2015-09-13 16:29:05 -0400

    Fix mixed indentation

--- app/workers/addon_data_updater.rb
+++ app/workers/addon_data_updater.rb
@@ -31,125 +31,125 @@ class AddonDataUpdater
 	end
 
 	def demo_url
-	  ember_addon_info = @metadata['latest']['ember-addon']
-	  return nil if ember_addon_info.nil?
-	  demo_url = ember_addon_info['demoURL']
-	  if demo_url.nil? || demo_url !~ /^http/
-	    return nil
-	  end
-	  demo_url
+		ember_addon_info = @metadata['latest']['ember-addon']
+		return nil if ember_addon_info.nil?
+		demo_url = ember_addon_info['demoURL']
+		if demo_url.nil? || demo_url !~ /^http/
+			return nil
+		end
+		demo_url
 	end
 
 	def repo_url
 		url = @metadata['repository']['url']
-	  return nil if url.nil?
-	  if url =~ %r|^http:///|
-	    url.sub!('http:///', 'http://')
-	  end
-	  if url =~ /git@github.com/
-	    url.sub!('git@github.com', 'github.com')
-	  end
-	  if url =~ %r|^git\+https://github.com|
-	    url.sub!(/^git\+/, '')
-	  elsif url =~ %r|^git\+ssh://github.com|
-	    url.sub!(/^git\+ssh/, 'https')
-	  end
-	  url.sub(/`$/, '')
+		return nil if url.nil?
+		if url =~ %r|^http:///|
+			url.sub!('http:///', 'http://')
+		end
+		if url =~ /git@github.com/
+			url.sub!('git@github.com', 'github.com')
+		end
+		if url =~ %r|^git\+https://github.com|
+			url.sub!(/^git\+/, '')
+		elsif url =~ %r|^git\+ssh://github.com|
+			url.sub!(/^git\+ssh/, 'https')
+		end
+		url.sub(/`$/, '')
 	end
 
 	# This is needed because sometimes NPM makes up crazy shit for data. In the instance
 	# that led to this, it decided to give back the repo name as repo-name.git#repo-name,
 	# which doesn't appear anywhere in the addon's package.json or README.
 	def unmangle_github_data(str)
-	  str = str.split(/#/)[0]
-	  str.sub!(/\.git$/, '')
-	  str.sub(/`$/, '')
+		str = str.split(/#/)[0]
+		str.sub!(/\.git$/, '')
+		str.sub(/`$/, '')
 	end
 
 	def update_addon_versions
 		current_versions = @metadata['versions'].keys
-	  @addon.addon_versions = AddonVersion.where(addon_id: @addon.id, version: current_versions)
+		@addon.addon_versions = AddonVersion.where(addon_id: @addon.id, version: current_versions)
 
-	  @metadata['versions'].each do |version, data|
-	    addon_version = @addon.addon_versions.find_by(version: version)
-	    if addon_version && data['devDependencies'] && data['devDependencies']['ember-cli'] && !addon_version.ember_cli_version
-	      addon_version.ember_cli_version = data['devDependencies']['ember-cli']
-	      addon_version.save
-	    end
-	    unless addon_version
-	      addon_version = AddonVersion.create!(
-	        addon: @addon,
-	        version: version,
-	        released: @metadata['time'][version],
-	        ember_cli_version: (data['devDependencies'] ? data['devDependencies']['ember-cli'] : nil)
-	      )
-	      @addon.addon_versions << addon_version
-	      update_addon_version_dependencies(addon_version, data)
-	    end
-	  end
+		@metadata['versions'].each do |version, data|
+			addon_version = @addon.addon_versions.find_by(version: version)
+			if addon_version && data['devDependencies'] && data['devDependencies']['ember-cli'] && !addon_version.ember_cli_version
+				addon_version.ember_cli_version = data['devDependencies']['ember-cli']
+				addon_version.save
+			end
+			unless addon_version
+				addon_version = AddonVersion.create!(
+					addon: @addon,
+					version: version,
+					released: @metadata['time'][version],
+					ember_cli_version: (data['devDependencies'] ? data['devDependencies']['ember-cli'] : nil)
+				)
+				@addon.addon_versions << addon_version
+				update_addon_version_dependencies(addon_version, data)
+			end
+		end
 	end
 
 	def update_addon_version_compatibility(addon_version, addon_version_metadata)
 		return unless addon_version_metadata.include?('ember-addon')
-	  return unless addon_version_metadata['ember-addon'].include?('versionCompatibility')
+		return unless addon_version_metadata['ember-addon'].include?('versionCompatibility')
 
-	  addon_version.compatible_versions.clear
+		addon_version.compatible_versions.clear
 
-	  addon_version_metadata['ember-addon']['versionCompatibility'].each do |package_name, package_version|
-	    version_compatibility = AddonVersionCompatibility.create(
-	      package: package_name,
-	      version: package_version
-	    )
-	    addon_version.compatible_versions << version_compatibility
-	  end
+		addon_version_metadata['ember-addon']['versionCompatibility'].each do |package_name, package_version|
+			version_compatibility = AddonVersionCompatibility.create(
+				package: package_name,
+				version: package_version
+			)
+			addon_version.compatible_versions << version_compatibility
+		end
 	end
 
 	def update_addon_version_dependencies(addon_version, addon_version_metadata)
 		addon_version.dependencies.clear
-	  [ 'devDependencies', 'dependencies', 'optionalDependencies', 'peerDependencies' ].each do |dependency_type|
-	    next unless addon_version_metadata[dependency_type]
-	    addon_version_metadata[dependency_type].each do |package_name, version|
-	      dependency = AddonVersionDependency.create(
-	        package: package_name,
-	        version: version,
-	        dependency_type: dependency_type,
-	        addon_version: addon_version
-	      )
-	      addon_version.dependencies << dependency
-	    end
+		[ 'devDependencies', 'dependencies', 'optionalDependencies', 'peerDependencies' ].each do |dependency_type|
+			next unless addon_version_metadata[dependency_type]
+			addon_version_metadata[dependency_type].each do |package_name, version|
+				dependency = AddonVersionDependency.create(
+					package: package_name,
+					version: version,
+					dependency_type: dependency_type,
+					addon_version: addon_version
+				)
+				addon_version.dependencies << dependency
+			end
 
-	    update_addon_version_compatibility(addon_version, addon_version_metadata)
-	  end
+			update_addon_version_compatibility(addon_version, addon_version_metadata)
+		end
 	end
 
 	def update_author
 		npm_author = @metadata['author']
-	  if npm_author
-	    author = NpmAuthor.find_or_create_by(name: npm_author['name'], email: npm_author['email'])
-	    author.url = npm_author['url']
-	    if author != @addon.author
-	      @addon.author = author
-	    end
-	    author.save
-	  else
-	    @addon.author = nil
-	  end
+		if npm_author
+			author = NpmAuthor.find_or_create_by(name: npm_author['name'], email: npm_author['email'])
+			author.url = npm_author['url']
+			if author != @addon.author
+				@addon.author = author
+			end
+			author.save
+		else
+			@addon.author = nil
+		end
 	end
 
 	def update_downloads
 		if @metadata['downloads']['start']
-	    addon_downloads = @addon.downloads.find_or_create_by(date: @metadata['downloads']['start'])
-	    addon_downloads.downloads = @metadata['downloads']['downloads']
-	    addon_downloads.save
-	  end
+			addon_downloads = @addon.downloads.find_or_create_by(date: @metadata['downloads']['start'])
+			addon_downloads.downloads = @metadata['downloads']['downloads']
+			addon_downloads.save
+		end
 	end
 
 	def update_keywords
 		@addon.npm_keywords.clear
-	  @metadata['keywords'].each do |keyword|
-	    npm_keyword = NpmKeyword.find_or_create_by(keyword: keyword)
-	    @addon.npm_keywords << npm_keyword
-	  end
+		@metadata['keywords'].each do |keyword|
+			npm_keyword = NpmKeyword.find_or_create_by(keyword: keyword)
+			@addon.npm_keywords << npm_keyword
+		end
 	end
 
 	def update_maintainers
