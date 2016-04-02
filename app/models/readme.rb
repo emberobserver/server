2a8e5b70dc701daf16117abeff7822f67b1a8a66
Phil Gengler <phil@pgengler.net>
2016-04-02 12:33:29 -0400

    Rerun 'annotate'

--- app/models/readme.rb
+++ app/models/readme.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: readmes
+#
+#  id       :integer          not null, primary key
+#  contents :text
+#  addon_id :integer
+#
+
 class Readme < ActiveRecord::Base
   belongs_to :addon
 end
