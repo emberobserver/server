d324c4d7a72c5b0799728a2aa14cb0725152907f
Michelle Santarsiero <michelle.santarsiero@gmail.com>
2016-07-26 12:38:37 -0400

    Update annotations

--- app/serializers/readme_serializer.rb
+++ app/serializers/readme_serializer.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: readmes
+#
+#  id       :integer          not null, primary key
+#  contents :text
+#  addon_id :integer
+#
+
 class ReadmeSerializer < ApplicationSerializer
   attributes :id, :contents
 
