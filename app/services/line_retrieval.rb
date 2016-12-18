826b4f5f20833780ff8c744effaff3e039876218
Michelle Santarsiero <michelle.santarsiero@gmail.com>
2016-12-18 14:02:34 -0500

    Code cleanup and small optimization

--- app/services/line_retrieval.rb
+++ app/services/line_retrieval.rb
@@ -3,12 +3,12 @@ class LineRetrieval
   def retrieve_match(filename, line_number)
     from_line = line_number - 3
     from_line = 1 if from_line < 1
+    to_line = line_number + 3
 
-    sed_command = "#{from_line},#{line_number+3}p;#{line_number + 4}q"
-    command = ['sed -n', "'#{sed_command}'", filename].join(' ')
+    sed_command = "#{from_line},#{to_line}p;#{to_line+1}q"
 
-    lines = `#{command}`
-    lines.split("\n").zip(from_line..line_number + 3)
+    lines = `sed -n '#{sed_command}' #{filename}`
+    lines.split("\n").zip(from_line..to_line)
   end
 
 end
