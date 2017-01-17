e80b2fb4fa65bbc417117da1843e1922ee46224a
Michelle Santarsiero <michelle.santarsiero@gmail.com>
2017-01-17 09:33:33 -0500

    Use IO.readlines to fetch usage context (faster and simpler)

---
+++ app/services/line_reader.rb
@@ -0,0 +1,12 @@
+class LineReader
+
+  def retrieve_context(filename, line_number)
+    from_line = line_number - 3
+    from_line = 1 if from_line < 1
+    to_line = line_number + 3
+
+    lines = IO.readlines(filename, "\n")
+    lines[from_line-1..to_line-1].zip(from_line..to_line)
+  end
+
+end
