5bd30f8d057e092b40a75e1190a2524f4a8c70b4
Katie Gengler <katie@kmg.io>
2017-02-19 17:47:35 -0500

    Only send cache header on certain status codes

--- app/controllers/api/v2/jsonapi_base_controller.rb
+++ app/controllers/api/v2/jsonapi_base_controller.rb
@@ -1,6 +1,6 @@
 class API::V2::JsonapiBaseController < JSONAPI::ResourceController
   include TokenAuth
-  before_action :allow_page_caching
+  after_action :allow_page_caching
 
   def context
     { current_user: current_user }
@@ -10,7 +10,9 @@ class API::V2::JsonapiBaseController < JSONAPI::ResourceController
 
   def allow_page_caching
     if current_user.nil? && request.get?
-      expires_in(1.hour, public: true)
+      if [200, 301, 302].include?(response.code)
+        expires_in(1.hour, public: true)
+      end
     end
   end
 end
