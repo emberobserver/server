f57c65071b2313952f9526a5cc3a9bf2ee0871b6
Phil Gengler <phil@pgengler.net>
2017-05-21 13:31:40 -0400

    Use a newer version of Node

--- server_setup/server_init.bash
+++ server_setup/server_init.bash
@@ -9,6 +9,7 @@ USER=eo
 GROUP=eo
 
 WEB_USER=www-data
+NODE_VERSION=$(cat .nvmrc)
 RUBY_VERSION=$(cat .ruby-version)
 RUBY_GEMSET=$(cat .ruby-gemset)
 
@@ -68,8 +69,9 @@ chown ${USER}.${GROUP} ~${USER}/.ssh
 echo "${PUBLIC_KEY}" > ~${USER}/.ssh/authorized_keys
 
 apt-get update
-apt-get -y install postgresql postgresql-contrib postgresql-server-dev-all nginx redis-server git nodejs npm nodejs-legacy golang-go
+apt-get -y install postgresql postgresql-contrib postgresql-server-dev-all nginx redis-server git golang-go
 
+# Install RVM and Ruby
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable --ruby=${RUBY_VERSION}
 source /usr/local/rvm/scripts/rvm
@@ -77,6 +79,13 @@ rvm gemset create "${RUBY_GEMSET}"
 gem install bundler
 rvm gemset use "${RUBY_GEMSET}"
 gem install bundler
+
+# Install NVM and Node
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | NVM_DIR=/usr/local/nvm bash
+source /usr/local/nvm/nvm.sh
+nvm install "${NODE_VERSION}"
+nvm alias default "${NODE_VERSION}"
+echo "source /usr/local/nvm/nvm.sh" >> ~${USER}/.bashrc
 
 mkdir -p "${CLIENT_ROOT}/badges"
 mkdir -p "${CLIENT_ROOT}/www"
