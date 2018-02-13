d7a6d7e01a6e11a75620d4ea2200638bbae095d3
Katie Gengler <katie@kmg.io>
2018-02-13 12:33:50 -0500

    Fixup rubocop

--- app/models/readme_view.rb
+++ app/models/readme_view.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # == Schema Information
 #
 # Table name: readmes_indexed_for_fts
--- test/factories/npm_authors.rb
+++ test/factories/npm_authors.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # == Schema Information
 #
 # Table name: npm_authors
@@ -10,7 +11,6 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
-
 
 FactoryGirl.define do
   factory :npm_author do
