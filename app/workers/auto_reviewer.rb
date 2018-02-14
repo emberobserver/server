d1903450d4378509738235a4f2b8e51a3bc7e897
Phil Gengler <phil@pgengler.net>
2018-02-13 21:06:44 -0500

    Extract rereview logic to a worker and add tests

---
+++ app/workers/auto_reviewer.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class AutoReviewer
+  def self.rereview_addons
+    # get addons needing re-review
+    addons = Addon.where('name in (?)', Review.select(:addon_name)).joins(:latest_addon_version).where('addon_versions.id NOT IN (?)', Review.select(:addon_version_id))
+
+    # filter list to addons with all 5 points on most recent reviews
+    addons = addons.select do |addon|
+      review = addon.newest_review
+      review.has_tests == 1 &&
+        review.has_readme == 1 &&
+        review.is_more_than_empty_addon == 1 &&
+        review.is_open_source == 1 &&
+        review.has_build == 1
+    end
+
+    addons.each do |addon|
+      # create all-yes review for newest version
+      review = addon.newest_review.dup
+      review.addon_version = addon.newest_version
+      review.save!
+    end
+  end
+end
