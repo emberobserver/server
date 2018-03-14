1dbc83d858a0d589e523492d4aa905a658d869c1
Katie Gengler <katie@kmg.io>
2018-03-14 14:29:10 -0700

    Fix test - Update rubocop

--- app/services/package_list_fetcher.rb
+++ app/services/package_list_fetcher.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class PackageListFetcher
-  FETCH_URL = "http://registry.npmjs.org/-/v1/search?text=keywords:ember-addon"
+  FETCH_URL = 'http://registry.npmjs.org/-/v1/search?text=keywords:ember-addon'
   PAGE_SIZE = 250
 
   class Request
@@ -45,7 +47,7 @@ class PackageListFetcher
       if response.success?
         parsed_response = parse_response(response.body)
 
-        unless parsed_response && parsed_response[:packages].length > 0
+        unless parsed_response && !parsed_response[:packages].empty?
           Rails.logger.warn("No packages returned when fetching addon list from npm #{request_params}")
           break
         end
@@ -57,7 +59,7 @@ class PackageListFetcher
           expected_count += total
         end
 
-        fetched_count = fetched_count + parsed_response[:packages].length
+        fetched_count += parsed_response[:packages].length
       elsif response.timed_out?
         Rails.logger.warn("Timed out fetching addon list from npm #{request_params}")
         break
@@ -75,95 +77,11 @@ class PackageListFetcher
     end
 
     matching_npm_packages
-
-  #
-  #   addons_needing_updating = []
-  #   new_addons = []
-  #   matching_npm_packages.each do |a|
-  #     addon_data = a["package"]
-  #     addon = Addon.find_by(name: addon_data["name"])
-  #
-  #     if !addon
-  #       new_addons << addon_data["name"]
-  #       next
-  #     end
-  #
-  #     if DateTime.parse(addon_data["date"]) > addon.latest_version_date
-  #       addons_needing_updating << addon_data["name"]
-  #     end
-  #   end
-  #
-  #
-  #   self.update_addons(addons_needing_updating.push(*new_addons))
-  #   pp addons_needing_updating: addons_needing_updating.length
-  #   pp new_addons: new_addons.length
-  #   nil
-  # end
-  #
-  # def self.update_addons(addon_names)
-  #   addon_names.each do |name|
-  #     base_url = "https://registry.npmjs.org/"
-  #     sanitized_name = name.gsub('/', '%2F')
-  #     request = Typhoeus::Request.new("#{base_url}#{sanitized_name}")
-  #     request.on_complete do |response|
-  #       if response.success?
-  #         begin
-  #           response_body = response.body
-  #           contents = JSON.parse(response_body)
-  #           NpmAddonDataUpdater.new(contents).update
-  #         rescue
-  #           pp failed: name
-  #         end
-  #       elsif response.timed_out?
-  #         # aw hell no
-  #         #log("got a time out")
-  #       elsif response.code == 0
-  #         # Could not get an http response, something's wrong.
-  #         #log(response.return_message)
-  #       else
-  #         # Received a non-successful http response.
-  #         #log("HTTP request failed: " + response.code.to_s)
-  #       end
-  #     end
-  #
-  #     request.run
-  #   end
   end
 
   def self.parse_response(response)
-    if response
-      contents = JSON.parse(response)
-      { packages: contents["objects"], total: contents["total"] }
-    end
+    return unless response
+    contents = JSON.parse(response)
+    { packages: contents['objects'], total: contents['total'] }
   end
 end
-
-
-# returns
-#
-# "package"=>
-#   {"name"=>"ember-cli-htmlbars",
-#    "scope"=>"unscoped",
-#    "version"=>"2.0.3",
-#    "description"=>"A library for adding htmlbars to ember CLI",
-#    "keywords"=>["ember-cli", "ember-addon"],
-#    "date"=>"2017-07-29T22:20:43.518Z",
-#    "links"=>
-#      {"npm"=>"https://www.npmjs.com/package/ember-cli-htmlbars",
-#       "homepage"=>"https://github.com/ember-cli/ember-cli-htmlbars",
-#       "repository"=>"https://github.com/ember-cli/ember-cli-htmlbars",
-#       "bugs"=>"https://github.com/ember-cli/ember-cli-htmlbars/issues"},
-#    "author"=>{"name"=>"Jonathan Jackson & Chase McCarthy"},
-#    "publisher"=>{"username"=>"rwjblue", "email"=>"me@rwjblue.com"},
-#    "maintainers"=>
-#      [{"username"=>"ember-cli", "email"=>"stefan.penner+ember-cli@gmail.com"},
-#       {"username"=>"rondale-sc", "email"=>"jonathan.jackson1@me.com"},
-#       {"username"=>"rwjblue", "email"=>"me@rwjblue.com"},
-#       {"username"=>"stefanpenner", "email"=>"stefan.penner@gmail.com"}]},
-#   "score"=>
-#   {"final"=>0.8002942748010851,
-#    "detail"=>
-#      {"quality"=>0.9531758371062433,
-#       "popularity"=>0.4696994768686256,
-#       "maintenance"=>0.9998477336148377}},
-#   "searchScore"=>0.04297416}
--- test/services/addons_updater_test.rb
+++ test/services/addons_updater_test.rb
@@ -10,36 +10,36 @@ class AddonsUpdaterTest < ActiveSupport::TestCase
     create(:addon, name: 'ember-power-select', latest_version_date: 3.days.ago.utc.iso8601)
     matching_npm_packages = [
       {
-        "package": {
-          "name": "ember-try",
-          "date": ember_try_date
+        'package' => {
+          'name' => 'ember-try',
+          'date' => ember_try_date
         }
       },
       {
-        "package": {
-          "name": "my-new-addon",
-          "date": DateTime.now.utc.iso8601
+        'package' => {
+          'name' => 'my-new-addon',
+          'date' => Time.zone.now.utc.iso8601
         }
       },
       {
-        "package": {
-          "name": "@ember/test-helpers",
-          "date": 1.day.ago.utc.iso8601
+        'package' => {
+          'name' => '@ember/test-helpers',
+          'date' => 1.day.ago.utc.iso8601
         }
       },
       {
-        "package": {
-          "name": "ember-power-select",
-          "date": 4.days.ago.utc.iso8601
+        'package' => {
+          'name' => 'ember-power-select',
+          'date' => 4.days.ago.utc.iso8601
         }
       }
     ]
 
     results = AddonsUpdater.addons_in_need_of_update(matching_npm_packages)
 
-    assert_equal(%w(
-      my-new-addon
-      @ember/test-helpers
-    ), results, "Includes addons that have been updated or are new")
+    assert_equal(%w[
+                   my-new-addon
+                   @ember/test-helpers
+                 ], results, 'Includes addons that have been updated or are new')
   end
 end
