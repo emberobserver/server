bed6c53db7eb8e9987018a30d10e797bc6349e19
Katie Gengler <katie@kmg.io>
2018-05-20 23:57:52 -0400

    Appease rubocop

--- app/workers/update_addon_worker.rb
+++ app/workers/update_addon_worker.rb
@@ -2,7 +2,7 @@
 
 class UpdateAddonWorker
   include Sidekiq::Worker
-  sidekiq_options :retry => 5
+  sidekiq_options retry: 5
 
   def perform(addon_name)
     data = PackageFetcher.run(addon_name)
