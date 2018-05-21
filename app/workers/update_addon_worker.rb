63457e6e7ac20ed1c6b59f295e429a6c6a8bd06d
Katie Gengler <katie@kmg.io>
2018-05-20 23:52:22 -0400

    Up db connection pool to account for sidekiq
    Refactor a bit

--- app/workers/update_addon_worker.rb
+++ app/workers/update_addon_worker.rb
@@ -2,8 +2,10 @@
 
 class UpdateAddonWorker
   include Sidekiq::Worker
+  sidekiq_options :retry => 5
 
   def perform(addon_name)
-    PackageFetcher.run(addon_name)
+    data = PackageFetcher.run(addon_name)
+    NpmAddonDataUpdater.new(data).update
   end
 end
