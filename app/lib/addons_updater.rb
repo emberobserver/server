2f787979d2a21b6e3ddaeac9754e863148a81350
Phil Gengler <phil@pgengler.net>
2018-07-17 20:37:47 -0400

    Fix error raising in AddonsUpdater

--- app/lib/addons_updater.rb
+++ app/lib/addons_updater.rb
@@ -5,7 +5,7 @@ class AddonsUpdater
     matching_npm_packages = PackageListFetcher.run
 
     if matching_npm_packages.length < 5000
-      raise RuntimeError("Did not find at least 5000 matching packages, found: #{matching_npm_packages.length}")
+      raise "Did not find at least 5000 matching packages, found: #{matching_npm_packages.length}"
     end
 
     addons_to_update = addons_in_need_of_update(matching_npm_packages)
