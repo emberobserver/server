81850ca1adde8e2e5d041837620df6b02f3c7354
Michelle Santarsiero <michelle.santarsiero@gmail.com>
2018-10-06 06:16:07 -0400

    Only perform update if install and measure step succeeds

--- app/services/addon_size_calculator.rb
+++ app/services/addon_size_calculator.rb
@@ -9,10 +9,10 @@ class AddonSizeCalculator
 
       begin
         diff = ember_new_output.install_addon_and_measure(addon_version)
+        AddonSizeUpdater.perform_async(addon_version.id, diff.to_h) if diff
       rescue StandardError => e
         Rails.logger.error("Failed to measure addon #{addon_version.addon_name}: #{e}")
       end
-      AddonSizeUpdater.perform_async(addon_version.id, diff.to_h) if diff
     end
     ember_new_output.cleanup
   end
