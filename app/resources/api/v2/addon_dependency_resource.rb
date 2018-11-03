82e1f7fda6c6718eaaf1914e583d2081028d38e7
Michelle Santarsiero <michelle.santarsiero@gmail.com>
2018-11-03 17:49:37 -0400

    Add endpoint for addon version dependencies
    Filter out dependencies that are not addons by default

---
+++ app/resources/api/v2/addon_dependency_resource.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class API::V2::AddonDependencyResource < JSONAPI::Resource
+  immutable
+  model_name 'AddonVersionDependency'
+
+  attributes :package, :dependency_type
+
+  has_one :dependent_version, class_name: 'Version', relation_name: 'addon_version', foreign_key: 'addon_version_id'
+
+  filter :addons_only, apply: ->(records, value, _options) {
+    addons_only = ActiveModel::Type::Boolean.new.cast(value[0])
+    if addons_only
+      records.where('package in (select name from addons)')
+    else
+      records
+    end
+  }, default: true
+
+  filter :addon_version_id
+  filter :dependency_type
+end
