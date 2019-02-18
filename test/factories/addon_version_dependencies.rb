ba18af139813f543e19af6a04bd4a9cc0d2b7ca2
Katie Gengler <katie@kmg.io>
2019-02-17 23:37:45 -0500

    Add generated schema annotations

--- test/factories/addon_version_dependencies.rb
+++ test/factories/addon_version_dependencies.rb
@@ -1,4 +1,27 @@
 # frozen_string_literal: true
+# == Schema Information
+#
+# Table name: addon_version_dependencies
+#
+#  id               :integer          not null, primary key
+#  package          :string
+#  version          :string
+#  dependency_type  :string
+#  addon_version_id :integer
+#  package_addon_id :integer
+#
+# Indexes
+#
+#  index_addon_version_dependencies_on_addon_version_id     (addon_version_id)
+#  index_addon_version_dependencies_on_package              (package)
+#  index_addon_version_dependencies_on_package_addon_id     (package_addon_id)
+#  index_addon_version_dependencies_on_package_and_version  (package,version)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (package_addon_id => addons.id)
+#
+
 
 FactoryBot.define do
   factory :addon_version_dependency do
