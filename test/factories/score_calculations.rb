c86ab2b296b613ae314bfabd7ddb0a8853587ac6
Katie Gengler <katie@kmg.io>
2019-03-05 16:37:54 -0500

    Store score_calculation in the database to preserve history

---
+++ test/factories/score_calculations.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: score_calculations
+#
+#  id               :integer          not null, primary key
+#  addon_id         :integer
+#  addon_version_id :integer
+#  info             :jsonb
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+# Indexes
+#
+#  index_score_calculations_on_addon_id          (addon_id)
+#  index_score_calculations_on_addon_version_id  (addon_version_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (addon_id => addons.id)
+#  fk_rails_...  (addon_version_id => addon_versions.id)
+#
+
+FactoryBot.define do
+  factory :score_calculation do
+  end
+end
---
+++ test/models/score_calculation_test.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: score_calculations
+#
+#  id               :integer          not null, primary key
+#  addon_id         :integer
+#  addon_version_id :integer
+#  info             :jsonb
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+# Indexes
+#
+#  index_score_calculations_on_addon_id          (addon_id)
+#  index_score_calculations_on_addon_version_id  (addon_version_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (addon_id => addons.id)
+#  fk_rails_...  (addon_version_id => addon_versions.id)
+#
+
+require 'test_helper'
+
+class ScoreCalculationTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
