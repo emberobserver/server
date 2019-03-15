287904cf274e8d0ed42008bd9a5d4e0602fc60c2
Phil Gengler <phil@pgengler.net>
2019-03-15 12:34:40 -0400

    Use rubocop-approved exception raising

--- app/resources/api/v2/maintainer_resource.rb
+++ app/resources/api/v2/maintainer_resource.rb
@@ -11,7 +11,7 @@ class API::V2::MaintainerResource < JSONAPI::Resource
     matching_records = records.where(name: values)
     unless matching_records.count == values.length
       missing_names = values - matching_records.map(&:name)
-      raise JSONAPI::Exceptions::RecordNotFound.new(missing_names.first)
+      raise JSONAPI::Exceptions::RecordNotFound, missing_names.first
     end
     matching_records
   }
