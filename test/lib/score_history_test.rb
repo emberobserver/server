c470a83861acc0fe92951673936eaf95bc901b7a
Phil Gengler <phil@pgengler.net>
2019-03-17 07:03:36 -0700

    Make ScoreHistory.rows_by_model_version nicely handle the fact that some addons may only have scores under some (but not all) model_versions

--- test/lib/score_history_test.rb
+++ test/lib/score_history_test.rb
@@ -61,11 +61,18 @@ class ScoreHistoryTest < ActiveSupport::TestCase
       score: 2.3, addon_name: 'ember-try', model_version: 2
     }
 
+    addon_c = create :addon
+    addon_version_c = create :addon_version, addon: addon_c
+    create :score_calculation, addon: addon_c, addon_version: addon_version_c, info: {
+      score: 7, addon_name: 'ember-newly-published-addon', model_version: 2
+    }
+
     data = ScoreHistory.rows_by_model_version
 
     assert_equal([
                    ['Addon', 'Model 1', 'Model 2'],
                    ['ember-info', 9, 9.2],
+                   ['ember-newly-published-addon', nil, 7],
                    ['ember-try', 3, 2.3]
                  ], data)
   end
