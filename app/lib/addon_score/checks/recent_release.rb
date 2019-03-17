2feafec506728933c764d1c531c4772203b261a1
Katie Gengler <katie@kmg.io>
2019-03-16 22:30:41 -0700

    Modify released check to give partial credit from 3 - 12months

--- app/lib/addon_score/checks/recent_release.rb
+++ app/lib/addon_score/checks/recent_release.rb
@@ -3,20 +3,23 @@
 module AddonScore
   module Checks
     class RecentRelease < Check
+      include ActionView::Helpers::DateHelper
+
       def name
         :recent_release
       end
 
       def explanation
-        if value == 1
-          'Has published a release on `npm` within the past three months'
+        since_words = time_ago_in_words(addon.latest_addon_version.released)
+        if value > 0
+          "Published a release to `npm` tagged `latest` within the past #{since_words}"
         else
-          'Has not published a release to `npm` within the past three months'
+          "Has not published a release to `npm` tagged `latest` within #{since_words}"
         end
       end
 
       def max_value
-        1
+        100
       end
 
       def weight
@@ -24,7 +27,12 @@ module AddonScore
       end
 
       def value
-        addon.recently_released? ? 1 : 0
+        return 0 unless addon.latest_addon_version
+        latest_release = addon.latest_addon_version.released
+        return 100 if latest_release >= 3.months.ago
+        return 80 if latest_release >= 6.months.ago
+        return 50 if latest_release >= 12.months.ago
+        0
       end
     end
   end
