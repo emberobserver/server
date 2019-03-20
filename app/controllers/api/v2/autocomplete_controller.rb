139cc77fc7429d097693bdefc6cafa66351c3fc3
Phil Gengler <phil@pgengler.net>
2019-03-20 11:39:12 -0700

    Use Addon.not_hidden scope for autocomplete, so we don't return addons removed from NPM

--- app/controllers/api/v2/autocomplete_controller.rb
+++ app/controllers/api/v2/autocomplete_controller.rb
@@ -3,7 +3,7 @@
 class API::V2::AutocompleteController < ApplicationController
   def data
     render json: {
-      addons: Addon.select(:id, :name, :description, :score, :is_wip).where(hidden: :false),
+      addons: Addon.not_hidden.select(:id, :name, :description, :score, :is_wip),
       categories: Category.select(:id, :name),
       maintainers: NpmMaintainer.select(:id, :name)
     }.as_json
