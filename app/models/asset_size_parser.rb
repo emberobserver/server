efa45f0a8a7cef03920e1a6325a9957fd060e210
Michelle Santarsiero <michelle.santarsiero@gmail.com>
2019-05-05 10:30:28 -0400

    Asset size parser handles non-json without hanging; return custom error

--- app/models/asset_size_parser.rb
+++ app/models/asset_size_parser.rb
@@ -1,14 +1,21 @@
 # frozen_string_literal: true
 
 class AssetSizeParser
+  class ParseError < StandardError ; end
+
   def initialize(file_contents)
     @file_contents = file_contents
   end
 
   def asset_size_json
     normalized_contents = file_contents.dup
+    raise ParseError unless normalized_contents.include? '{'
     normalized_contents.sub!(/^(.*?)\n/, '') until normalized_contents.starts_with? '{'
-    JSON.parse(normalized_contents)
+    begin
+      JSON.parse(normalized_contents)
+    rescue
+      raise ParseError
+    end
   end
 
   attr_reader :file_contents
