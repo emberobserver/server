61d716c027f3aba7b6216bf557dbd983d5e8a935
Michelle Santarsiero <michelle.santarsiero@gmail.com>
2019-05-05 13:55:35 -0400

    Add method to convert size diffs to json
    AddonSizeDiffs will be sent to size calculations controller as json in this format

--- test/models/addon_size_diff_test.rb
+++ test/models/addon_size_diff_test.rb
@@ -100,4 +100,32 @@ class AddonSizeDiffTest < ActiveSupport::TestCase
     diff = AddonSizeDiff.new(base_sizes, new_sizes)
     assert_equal 1146477, diff.other_js
   end
+
+  test 'diffs can be converted to json' do
+    base_sizes = { 'files' => [
+      { 'name' => 'dist/assets/my-app-4de90fc813bff2b33b61424f89ab694f.js', 'size' => 4356, 'gzipSize' => 1315 },
+      { 'name' => 'dist/assets/my-app-d41d8cd98f00b204e9800998ecf8427e.css', 'size' => 0, 'gzipSize' => 20 },
+      { 'name' => 'dist/assets/vendor-a6d7c6355370786667cbe7d22c954542.js', 'size' => 752816, 'gzipSize' => 192342 },
+      { 'name' => 'dist/assets/vendor-d41d8cd98f00b204e9800998ecf8427e.css', 'size' => 0, 'gzipSize' => 20 }
+    ] }
+
+    new_sizes = { 'files' => [
+      { 'name' => 'dist/assets/my-app-3b7e1b1790a262fb98900004fe95f989.js', 'size' => 6316, 'gzipSize' => 1446 },
+      { 'name' => 'dist/assets/my-app-d41d8cd98f00b204e9800998ecf8427e.css', 'size' => 0, 'gzipSize' => 20 },
+      { 'name' => 'dist/assets/vendor-7b7fa6908e8cf411d0a74286b5911772.js', 'size' => 767098, 'gzipSize' => 196131 },
+      { 'name' => 'dist/assets/vendor-d41d8cd98f00b204e9800998ecf8427e.css', 'size' => 0, 'gzipSize' => 20 },
+      { 'name' => 'dist/assets/intl-4d7d43141e437dac353033ef26ee0bf4.js', 'size' => 40783, 'gzipSize' => 13637 },
+      { 'name' => 'dist/assets/intl/intl.complete-f0ad87362f2317fce81ddadbcb15c7bc.js', 'size' => 934663, 'gzipSize' => 191766 },
+    ] }
+
+    diff = AddonSizeDiff.new(base_sizes, new_sizes)
+    json = diff.to_json
+
+    assert_equal json['appJsSize'], diff.app_js
+    assert_equal json['appCssSize'], diff.app_css
+    assert_equal json['vendorJsSize'], diff.vendor_js
+    assert_equal json['vendorCssSize'], diff.vendor_css
+    assert_equal json['otherJsSize'], diff.other_js
+    assert_equal json['otherCssSize'], diff.other_css
+  end
 end
