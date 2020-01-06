ad8f06605616385363e14cfd02d42bd57bcdc279
Katie Gengler <katie@kmg.io>
2020-01-06 01:51:06 -0500

    Audit more rules

--- linting/static-eslintrc.js
+++ linting/static-eslintrc.js
@@ -17,6 +17,16 @@ module.exports = {
   rules: {
     'ember/no-jquery': 'error',
     'ember/no-jquery-integration': 'error',
-    'ember/no-observers': 'error'
+    'ember/no-observers': 'error',
+    'ember/no-new-mixins': 'error',
+    'ember/no-old-shims': 'error',
+    'ember/no-tracked': 'error',
+    'ember/no-classic-classes': 'error',
+    'ember/no-classic-components': 'error',
+    'ember/no-computed-properties-in-native-classes': 'error',
+    'ember/no-get': 'error',
+    'ember/no-get-with-default': 'error',
+    'ember/no-ember-components': 'error',
+    'ember/no-glimmer-components': 'error'
   },
 };
