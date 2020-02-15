084b29f22265a583b77c961ef8490fbec776c5dc
Phil Gengler <phil@pgengler.net>
2020-02-15 10:39:53 -0500

    Quickly add a way to exclude certain packages from NPM fetch due to an NPM bug that's causing errors on our end

--- app/workers/update_addon_worker.rb
+++ app/workers/update_addon_worker.rb
@@ -4,7 +4,16 @@ class UpdateAddonWorker
   include Sidekiq::Worker
   sidekiq_options retry: 5
 
+  # List of package names that should be skipped. This is needed because some
+  # packages get returned in the package list but then aren't available to fetch
+  # as an individual package. (This is a bug in NPM.)
+  SKIPPED_PACKAGES = %w[
+    @choiceform/ui-foundation
+  ].freeze
+
   def perform(addon_name)
+    return if SKIPPED_PACKAGES.include?(addon_name)
+
     data = PackageFetcher.run(addon_name)
     addon = NpmAddonDataUpdater.new(data).update
     AddonScoreWorker.perform_async(addon.id)
