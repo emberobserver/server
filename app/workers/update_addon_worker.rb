052e23c97b7a629d9905ca73d1cc3ca15e37079c
Phil Gengler <phil@pgengler.net>
2020-05-10 20:51:48 -0400

    Add more entries to SKIPPED_PACKAGES

--- app/workers/update_addon_worker.rb
+++ app/workers/update_addon_worker.rb
@@ -8,7 +8,9 @@ class UpdateAddonWorker
   # packages get returned in the package list but then aren't available to fetch
   # as an individual package. (This is a bug in NPM.)
   SKIPPED_PACKAGES = %w[
+    @choiceform/components-display
     @choiceform/components-feedback
+    @choiceform/components-inputs
     @choiceform/components-navigation
     @choiceform/ui-foundation
     @unihorncorn/horn-styles
