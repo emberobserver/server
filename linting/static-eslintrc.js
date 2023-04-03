933b47a1c8458b01389638b45bac44e6f39d2aae
Chris Manson <chris@manson.ie>
2023-04-03 16:05:35 +0100

    move ember-observer lint rules to new plugin

--- linting/static-eslintrc.js
+++ linting/static-eslintrc.js
@@ -9,24 +9,25 @@ module.exports = {
     }
   },
   plugins: [
-    'ember'
+    'ember',
+    'ember-observer'
   ],
   env: {
     browser: true
   },
   rules: {
     'ember/no-jquery': 'error',
-    'ember/no-jquery-integration': 'error',
     'ember/no-observers': 'error',
     'ember/no-new-mixins': 'error',
     'ember/no-old-shims': 'error',
-    'ember/no-tracked': 'error',
     'ember/no-classic-classes': 'error',
     'ember/no-classic-components': 'error',
     'ember/no-computed-properties-in-native-classes': 'error',
     'ember/no-get': 'error',
     'ember/no-get-with-default': 'error',
-    'ember/no-ember-components': 'error',
-    'ember/no-glimmer-components': 'error'
+    'ember-observer/no-ember-components': 'error',
+    'ember-observer/no-glimmer-components': 'error',
+    'ember-observer/no-jquery-integration': 'error',
+    'ember-observer/no-tracked': 'error',
   },
 };
